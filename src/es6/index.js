7cac923a3da5dfa87eb816c0140aa5852318d0fe
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-05-06 18:17:25 -0500

    Clase: Arrow Functions, Promesas y Parámetros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -16,7 +16,7 @@ newFunction2('Ricardo', 23, 'CO');
 
 
 // Template literals
-let hello ="Hello";
+let hello = "Hello";
 let world = "world";
 let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
@@ -26,7 +26,7 @@ console.log(epicPhrase2);
 // clase 2
 // saltos de linea en strings
 let lorem = "lorem ipsum dolorem\n"
-+ "Otra frase epica que necesitamos.";
+  + "Otra frase epica que necesitamos.";
 
 let lorem2 = `otra frase epica que necesitamos
 ahora es otra frase epica
@@ -37,7 +37,7 @@ console.log(lorem2);
 
 // Desestructurar objetos
 let person = {
-  'name' : 'Oscar',
+  'name': 'Oscar',
   'age': 32,
   'country': 'MX',
 };
@@ -70,3 +70,53 @@ console.log(global_var);
 // const
 const a = 'b';
 a = 'a';
+
+// Clase: Arrow Functions, Promesas y Parámetros en objetos
+let name = 'Oscar';
+let age = 21;
+// es5
+obj = { name: name, age: age };
+// es6
+obj2 = { name, age }
+
+console.log(obj);
+console.log(obj2);
+
+// Arrow functions
+const names = [
+  { name: 'Oscar', age: 31 },
+  { name: 'Pedro', age: 32 },
+]
+
+// es5
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+})
+
+//es6
+let listOfNames2 = names.map(item => console.log(item.name))
+
+const listOfNames3 = (name, ages, country) => {
+  //
+}
+
+const listOfNames4 = name => {
+  // 
+}
+
+const sqare = num => num * num;
+
+// Promesas
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!');
+    } else {
+      reject('Ups!!');
+    }
+  });
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .catch(error => console.log(error));
