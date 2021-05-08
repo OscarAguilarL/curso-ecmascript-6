170f99131e7ddd659bfd833b5fb4e3136370c072
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-05-08 16:13:49 -0500

    Async & await

--- src/es8/index.js
+++ src/es8/index.js
@@ -27,3 +27,30 @@ console.log(string.padEnd(12, ' -----'));
 const obj = {
   name: 'Oscar',
 }
+
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (false)
+      ? setTimeout(() => resolve('Hello world'), 3000)
+      : reject(new Error('Test error'));
+  })
+}
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+anotherFunction();
