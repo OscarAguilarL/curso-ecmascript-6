97b2fa237fe737cad732bf713fe5a1b6c504dc69
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-05-08 16:04:27 -0500

    Ecmascript 7

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3, 5, 6, 8, 7]
+
+if (numbers.includes(7)) {
+  console.log("Si se encuentra el valor 7");
+} else {
+  console.log("No se encuentra");
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
---
+++ src/es8/index.js
@@ -0,0 +1,29 @@
+const data = {
+  frontend: 'Oscar',
+  backend: 'Caroly',
+  desing: 'Ethan',
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+// object values
+const data = {
+  frontend: 'Oscar',
+  backend: 'Caroly',
+  desing: 'Ethan',
+}
+
+const values = Object.values(data)
+console.log(values);
+console.log(values.length);
+
+const string = "Hello";
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' -----'));
+
+
+const obj = {
+  name: 'Oscar',
+}
